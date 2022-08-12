fde45d8d75e6c0dcfb487b67921e4441bfba3306
dubasir <dubasir@gmail.com>
2022-08-12 14:05:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector from urllib.error import URLError
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
